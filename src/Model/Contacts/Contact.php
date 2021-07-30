c5e2e54d30f90dd2cbd081325068cb6462fe8f57
gotoroho <gotoroho@yandex.ru>
2021-07-30 16:15:16 +0300

    fix: replace find func

--- src/Model/Contacts/Contact.php
+++ src/Model/Contacts/Contact.php
@@ -11,19 +11,6 @@ class Contact extends Model
 {
     protected string $path = "contacts";
 
-    public function findByEmail(string $email): array
-    {
-        $filterQuery = Query::fromArray([
-            "email" => $email
-        ]);
-
-        $request = new Request($this->url . $filterQuery, $this->token);
-
-        $response = $request->setCustomRequest("GET")->exec();
-
-        return $response->getDataArray();
-    }
-
     public function create(ContactDto $contactDto): array
     {
         $request = new Request($this->url, $this->token);
@@ -36,6 +23,19 @@ class Contact extends Model
                 "phone" => $contactDto->getTelephone(),
             ]
         ]))->exec();
+
+        return $response->getDataArray();
+    }
+
+    public function findByEmail(string $email): array
+    {
+        $filterQuery = Query::fromArray([
+            "email" => $email
+        ]);
+
+        $request = new Request($this->url . $filterQuery, $this->token);
+
+        $response = $request->setCustomRequest("GET")->exec();
 
         return $response->getDataArray();
     }
