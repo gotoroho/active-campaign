a25290f925aeb8eb56bceafc1795925f1734cfa8
gotoroho <gotoroho@yandex.ru>
2021-08-02 15:16:04 +0300

    feat: new main functions

--- src/ActiveCampaign.php
+++ src/ActiveCampaign.php
@@ -3,22 +3,48 @@
 namespace Gotoroho\ActiveCampaign;
 
 use Gotoroho\ActiveCampaign\Model\Contacts\Contact;
+use Gotoroho\ActiveCampaign\Model\Contacts\ContactTag;
 use Gotoroho\ActiveCampaign\Model\Contacts\Dto\ContactDto;
+use Gotoroho\ActiveCampaign\Model\Contacts\Dto\ContactTagDto;
+use Gotoroho\ActiveCampaign\Model\Dto\TagDto;
+use Gotoroho\ActiveCampaign\Model\ECommerce\Connection;
 use Gotoroho\ActiveCampaign\Model\ECommerce\Customer;
 use Gotoroho\ActiveCampaign\Model\ECommerce\Dto\CustomerDto;
+use Gotoroho\ActiveCampaign\Model\Tag;
 
 class ActiveCampaign
 {
     public function __construct(string $url, string $token)
     {
         Config::init($url, $token);
+
+        $connection = new Connection();
+        Config::setConnectionId((int)$connection->findOrCreate()['id']);
     }
 
-    public function saveContact(ContactDto $contactDto)
+    public function saveContact(ContactDto $contactDto): array
     {
-        $contact = new Contact();
+        $contactModel = new Contact();
 
-        return $contact->findOrCreate($contactDto);
+        return $contactModel->findOrCreate($contactDto);
+    }
+
+    /**
+     * @param int $contactId
+     * @param array $tags = ['Opencart', 'Poland']
+     */
+    public function saveContactTags(int $contactId, array $tags): void
+    {
+        $contactTagModel = new ContactTag();
+        $tagModel = new Tag();
+
+        foreach ($tags as $tag) {
+            $tagDto = new TagDto($tag, $tag);
+            $tag = $tagModel->findOrCreate($tagDto);
+
+            $contactTagDto = new ContactTagDto($contactId, (int)$tag['id']);
+            $contactTagModel->create($contactTagDto);
+        }
     }
 
     public function saveCustomer(CustomerDto $customerDto)
@@ -26,18 +52,5 @@ class ActiveCampaign
         $customer = new Customer();
 
         return $customer->findOrCreate($customerDto);
-    }
-
-    public function test()
-    {
-        $customer = new Customer();
-        return $customer->test();
-
-//        $contact = new Contact();
-//        return $contact->findByEmail($email);
-//        return $contact->delete(79852);
-
-//        $connection = new Connection();
-//        return $connection->listAll();
     }
 }
