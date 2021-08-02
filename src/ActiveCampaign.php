4719d6b5e9b495b4a16bfe510df9c7ba9c979700
gotoroho <gotoroho@yandex.ru>
2021-08-02 11:54:15 +0300

    fix: main class initialisation

--- src/ActiveCampaign.php
+++ src/ActiveCampaign.php
@@ -7,19 +7,15 @@ use Gotoroho\ActiveCampaign\Model\Contacts\Dto\ContactDto;
 
 class ActiveCampaign
 {
-    private string $url;
-    private string $token;
-
     public function __construct(string $url, string $token)
     {
-        $this->url = $url;
-        $this->token = $token;
+        Config::init($url, $token);
     }
 
     public function saveContact(ContactDto $contactDto)
     {
-        $customer = new Contact($this->token, $this->url);
+        $contact = new Contact();
 
-        return $customer->findOrCreate($contactDto);
+        return $contact->findOrCreate($contactDto);
     }
 }
